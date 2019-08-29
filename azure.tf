0e4410c03d77406099949755247e8cfd24a4843f
hschoi <chs2251@sk.com>
2019-08-29 16:37:01 +0900

    Update azure.tf

--- azure.tf
+++ azure.tf
@@ -45,10 +45,10 @@ resource "azurerm_virtual_network" "vnetwork" {
     
 }
 resource "azurerm_subnet" "mysubnet" {
-    name = "MySubnetuuser25final"
+    name = "MySubnetuser25final"
     resource_group_name = "${azurerm_resource_group.rg.name}"
     virtual_network_name = "${azurerm_virtual_network.vnetwork.name}"
-    network_security_group_id = "${azurerm_network_security_group.secGroup.id}"
+    azurerm_subnet_network_security_group_association = "${azurerm_network_security_group.secGroup.id}"
     address_prefix = "45.0.1.0/24"
 }
 resource "azurerm_public_ip" "publicdomainip" {
